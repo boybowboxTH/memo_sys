6f329659005567479dc6f74d83964ad25ff57980
boybowboxTH <User@DESKTOP-HV5A2GJ>
2024-05-14 18:35:45 +0700

    test

---
+++ src/component/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note1 from "./Note1";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note1 />
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ src/component/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Footer() {
+  const time = new Date().getFullYear();
+  return <p>copy right{time}</p>;
+}
+export default Footer;
